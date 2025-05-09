5d234f2d54b164abee07d3b59245bc31747d6882
trx <psielta@gmail.com>
2025-05-08 23:08:53 -0300

    X

--- front/rxjs/store.ts
+++ front/rxjs/store.ts
@@ -2,16 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import { createEpicMiddleware } from "redux-observable";
 import { rootEpic } from "./rootEpic";
 import { cidadeSlice } from "./cidade/cidade.slice";
-import { movimentosEstoqueSlice } from "./movimentos-estoque/movimentos-estoque.slice";
-import { pedidosSlice } from "./pedidos/pedido.slice";
 import { actionTracker } from "./actionTracker";
 const epicMiddleware = createEpicMiddleware();
 
 export const store = configureStore({
   reducer: {
     cidade: cidadeSlice.reducer,
-    movimentosEstoque: movimentosEstoqueSlice.reducer,
-    pedidos: pedidosSlice.reducer,
   },
   middleware: (gDM) => gDM().concat(actionTracker).prepend(epicMiddleware),
   devTools: true,

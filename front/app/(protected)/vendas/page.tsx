4409a588c02929cfb81295d4a4ef795d741a3ea8
trx <psielta@gmail.com>
2025-05-19 17:54:56 -0300

    fix page.tsx

--- front/app/(protected)/vendas/page.tsx
+++ front/app/(protected)/vendas/page.tsx
@@ -1,6 +1,9 @@
 import { User } from "@/context/auth-context";
 import { apiServer } from "@/lib/api-server";
-import { ProdutoResponse } from "@/rxjs/produto/produto.model";
+import {
+  ProdutoListResponse,
+  ProdutoResponse,
+} from "@/rxjs/produto/produto.model";
 import { redirect } from "next/navigation";
 import React from "react";
 import Vendas from "./vendas";
@@ -11,8 +14,19 @@ async function page() {
   if (user.admin !== 1 && (user.permission_vendas ?? 0) !== 1) {
     redirect("/sem-permissao");
   }
-  let produtos = await apiServer<ProdutoResponse[]>("/produtos");
-  return <Vendas produtos={produtos} user={user} />;
+  let page: ProdutoListResponse | null | undefined = null;
+  try {
+    page = await apiServer<ProdutoListResponse | null | undefined>(
+      "/produtos?limit=2147483647"
+    );
+  } catch (error) {
+    console.error(error);
+  }
+  let produtodata: ProdutoResponse[] = [];
+  if (page) {
+    produtodata = page.produtos;
+  }
+  return <Vendas produtos={produtodata} user={user} />;
 }
 
 export default page;
